8185083059df317d15abbdc95ce4859418f2a427
Greg Domzalski <greg@criticalshift.io>
2024-10-30 00:29:31 +0000

    Add support for ucm-imx8m-plus board

--- target/linux/imx/image/imx8.mk
+++ target/linux/imx/image/imx8.mk
@@ -15,49 +15,72 @@ define Device/Default
 endef
 
 define Device/imx8mplus
-  DEVICE_VENDOR := NXP
-  DEVICE_MODEL := IMX8MPLUS
-  DEVICE_VARIANT := SD Card Boot
-  PLAT := iMX8MP
-  SOC_TYPE := iMX8M
-  DEVICE_TYPE := flash_evk
-  ENV_NAME:=imx8mp-sdboot
-  DEVICE_PACKAGES += \
-	firmware-imx \
-	imx-mkimage \
-	u-boot-imx8mp
-  DEVICE_DTS := freescale/imx8mp-evk
-  IMAGE/sdcard.img := \
-	imx-compile-dtb $$(DEVICE_DTS) | \
-	imx-create-flash $$(PLAT) $$(DEVICE_TYPE) | \
-	imx-clean | \
-	imx-append-sdhead $(1) | pad-to 32K | \
-	imx-append-boot $$(SOC_TYPE) | pad-to 4M | \
-	imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
-	imx-append-kernel $$(DEVICE_DTS) | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
-	append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
+	DEVICE_VENDOR := NXP
+	DEVICE_MODEL := IMX8MPLUS
+	DEVICE_VARIANT := SD Card Boot
+	PLAT := iMX8MP
+	SOC_TYPE := iMX8M
+	DEVICE_TYPE := flash_evk
+	ENV_NAME:=imx8mp-sdboot
+	DEVICE_PACKAGES += \
+		firmware-imx \
+		imx-mkimage \
+		u-boot-imx8mp
+	DEVICE_DTS := freescale/imx8mp-evk
+	IMAGE/sdcard.img := \
+		imx-compile-dtb $$(DEVICE_DTS) | \
+		imx-create-flash $$(PLAT) $$(DEVICE_TYPE) | \
+		imx-clean | \
+		imx-append-sdhead $(1) | pad-to 32K | \
+		imx-append-boot $$(SOC_TYPE) | pad-to 4M | \
+		imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
+		imx-append-kernel $$(DEVICE_DTS) | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
+		append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
 endef
 TARGET_DEVICES += imx8mplus
 
 define Device/iot-gate-imx8plus
-  DEVICE_VENDOR := NXP
-  DEVICE_MODEL := IMX8MPLUS CompuLab IOT-GATE i.MX8P
-  DEVICE_VARIANT := SD Card Boot
-  PLAT := iMX8MP
-  SOC_TYPE := iMX8M
-  DEVICE_TYPE := flash_evk
-  ENV_NAME:=iot-gate-imx8plus
-  DEVICE_PACKAGES += \
-	firmware-imx \
-	u-boot-compulab
-  DEVICE_DTS := compulab/iot-gate-imx8plus
-  IMAGE/sdcard.img := \
-	imx-compile-dtb $$(DEVICE_DTS) | \
-	imx-clean | \
-	imx-append-sdhead $(1) | pad-to 32K | \
-	imx-append-boot $$(SOC_TYPE) | pad-to 4M | \
-	imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
-	imx-append-kernel $$(DEVICE_DTS) | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
-	append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
+	DEVICE_VENDOR := NXP
+	DEVICE_MODEL := IMX8MPLUS CompuLab IOT-GATE i.MX8P
+	DEVICE_VARIANT := SD Card Boot
+	PLAT := iMX8MP
+	SOC_TYPE := iMX8M
+	DEVICE_TYPE := flash_evk
+	ENV_NAME:=iot-gate-imx8plus
+	DEVICE_PACKAGES += \
+		firmware-imx \
+		u-boot-compulab
+	DEVICE_DTS := compulab/iot-gate-imx8plus
+	IMAGE/sdcard.img := \
+		imx-compile-dtb $$(DEVICE_DTS) | \
+		imx-clean | \
+		imx-append-sdhead $(1) | pad-to 32K | \
+		imx-append-boot $$(SOC_TYPE) | pad-to 4M | \
+		imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
+		imx-append-kernel $$(DEVICE_DTS) | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
+		append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
 endef
 TARGET_DEVICES += iot-gate-imx8plus
+
+define Device/ucm-imx8m-plus
+	DEVICE_VENDOR := NXP
+	DEVICE_MODEL := CompuLab ucm-imx8m-plus
+	DEVICE_VARIANT := Boot Agnostic
+	PLAT := iMX8MP
+	SOC_TYPE := iMX8M
+	DEVICE_TYPE := flash_evk
+	ENV_NAME:=ucm-imx8m-plus
+	DEVICE_PACKAGES += \
+		firmware-imx \
+		u-boot-compulab
+	DEVICE_DTS := compulab/ucm-imx8m-plus
+	IMAGE/sdcard.img := \
+		imx-compile-dtb $$(DEVICE_DTS) | \
+		imx-clean | \
+		imx-append-sdhead $(1) | pad-to 32K | \
+		imx-append-boot $$(SOC_TYPE) | pad-to 4M | \
+		imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
+		imx-append-kernel $$(DEVICE_DTS) | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
+	append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
+endef
+TARGET_DEVICES += ucm-imx8m-plus
