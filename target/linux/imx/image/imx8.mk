aef743ef982956914c558205185e6d54c5a9a43d
Greg Domzalski <greg@criticalshift.io>
2024-11-05 23:46:11 +0000

    Updates and simplifications

--- target/linux/imx/image/imx8.mk
+++ target/linux/imx/image/imx8.mk
@@ -14,31 +14,6 @@ define Device/Default
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
-define Device/imx8mplus
-	DEVICE_VENDOR := NXP
-	DEVICE_MODEL := IMX8MPLUS
-	DEVICE_VARIANT := SD Card Boot
-	PLAT := iMX8MP
-	SOC_TYPE := iMX8M
-	DEVICE_TYPE := flash_evk
-	ENV_NAME:=imx8mp-sdboot
-	DEVICE_PACKAGES += \
-		firmware-imx \
-		imx-mkimage \
-		u-boot-imx8mp
-	DEVICE_DTS := freescale/imx8mp-evk
-	IMAGE/sdcard.img := \
-		imx-compile-dtb $$(DEVICE_DTS) | \
-		imx-create-flash $$(PLAT) $$(DEVICE_TYPE) | \
-		imx-clean | \
-		imx-append-sdhead $(1) | pad-to 32K | \
-		imx-append-boot $$(SOC_TYPE) | pad-to 4M | \
-		imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
-		imx-append-kernel $$(DEVICE_DTS) | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
-		append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
-endef
-TARGET_DEVICES += imx8mplus
-
 define Device/iot-gate-imx8plus
 	DEVICE_VENDOR := NXP
 	DEVICE_MODEL := IMX8MPLUS CompuLab IOT-GATE i.MX8P
@@ -74,7 +49,7 @@ define Device/ucm-imx8m-plus
 		firmware-imx \
 		u-boot-compulab \
 		kmod-mwifiex-iw612-sdio
-	DEVICE_DTS := compulab/ucm-imx8m-plus
+	DEVICE_DTS := compulab/ucm-imx8m-plus-shield-evk
 	IMAGE/sdcard.img := \
 		imx-compile-dtb $$(DEVICE_DTS) | \
 		imx-clean | \
